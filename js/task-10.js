a43b780a673305d37348f24c2415ecfacce1e3c1
amosovandrey <rivercity.dp@gmail.com>
2023-05-19 19:16:40 +0300

    task 10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,45 +1,33 @@
-// Задание 10 (выполнять не обязательно)
-// Напиши скрипт создания и очистки коллекции элементов.
-// Пользователь вводит количество элементов в input
-// и нажимает кнопку Создать, после чего рендерится коллекция.
-// При нажатии на кнопку Очистить, коллекция элементов очищается.
+const inputEl = document.querySelector('#controls input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
 
-// <div id="controls">
-//   <input type="number" min="1" max="100" step="1" />
-//   <button type="button" data-create>Create</button>
-//   <button type="button" data-destroy>Destroy</button>
-// </div>
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
 
-// <div id="boxes"></div>
+function createBoxes() {
+  const amount = inputEl.value;
+  const boxes = [];
 
-// Создай функцию createBoxes(amount), которая принимает
-// один параметр - число.Функция создает столько < div >
-// , сколько указано в amount и добавляет их в div#boxes.
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    const size = 30 + i * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
 
-// Размеры самого первого <div> - 30px на 30px.
-// Каждый элемент после первого, должен быть шире и
-// выше предыдущего на 10px.
-// Все элементы должены иметь случайный цвет фона в
-// формате HEX.Используй готовую функцию getRandomHexColor для
-//  получения цвета.
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+  boxesContainer.append(...boxes);
+}
 
-// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes,
-//  тем самым удаляя все созданные элементы.
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
-
-const inputEl = document.querySelector('input');
-const createBtn = document.querySelector('[data-create]');
-const destroyBtn = document.querySelector('[data-destroy]');
-const boxesEl = document.getElementById('boxes');
-
-function createBoxes(amount) {}
